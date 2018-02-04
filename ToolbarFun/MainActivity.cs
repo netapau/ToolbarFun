885fe786ee2d003ff0abef01eccc3335bd361371
Tony Simoes <sr.tony@gmail.com>
2018-02-04 18:46:04 +0100

    # ToolbarFun example :
    * cf: https://developer.xamarin.com/guides/android/user_interface/controls/tool-bar/replacing-the-action-bar/
    * test ok on API 21 : 04/02/2018

--- ToolbarFun/MainActivity.cs
+++ ToolbarFun/MainActivity.cs
@@ -1,6 +1,10 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
+
+// cf: https://developer.xamarin.com/guides/android/user_interface/controls/tool-bar/replacing-the-action-bar/
+// test ok on API 21 : 04/02/2018
 
 namespace ToolbarFun
 {
@@ -13,6 +17,23 @@ namespace ToolbarFun
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "My Toolbar";
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
--- ToolbarFun/Resources/Resource.Designer.cs
+++ ToolbarFun/Resources/Resource.Designer.cs
@@ -41,11 +41,42 @@ namespace ToolbarFun
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f070001
+			public const int MyButton = 2131165185;
+			
+			// aapt resource value: 0x7f070002
+			public const int menu_edit = 2131165186;
+			
+			// aapt resource value: 0x7f070004
+			public const int menu_preferences = 2131165188;
+			
+			// aapt resource value: 0x7f070003
+			public const int menu_save = 2131165187;
+			
+			// aapt resource value: 0x7f070000
+			public const int toolbar = 2131165184;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int toolbar = 2130903041;
 			
 			static Layout()
 			{
@@ -57,11 +88,58 @@ namespace ToolbarFun
 			}
 		}
 		
+		public partial class Menu
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int top_menus = 2131099648;
+			
+			static Menu()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Menu()
+			{
+			}
+		}
+		
+		public partial class Mipmap
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int ic_action_content_create = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int ic_action_content_save = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int ic_menu_copy_holo_dark = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int ic_menu_cut_holo_dark = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int ic_menu_paste_holo_dark = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int ic_menu_search_holo_dark = 2130837509;
+			
+			static Mipmap()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Mipmap()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
@@ -69,6 +147,22 @@ namespace ToolbarFun
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int MyTheme = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
